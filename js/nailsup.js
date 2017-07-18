fd16f65f2e3937691e64ee5c3830c03ab9f7546f
Julio Lima <julio_flima@hotmail.com>
2017-07-18 16:39:03 -0300

    start

---
+++ js/nailsup.js
@@ -0,0 +1,22 @@
+function Scrolldown() {
+    window.scroll(0, 300);
+}
+
+window.onload = Scrolldown;
+
+if (document.readyState) {
+    document.onreadystatechange = checkstate;
+} else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", saydone, false);
+}
+
+function checkstate() {
+    if (document.readyState == "complete" || document.readyState == "complete") {
+        document.getElementById("page").style.display = "block";
+    }
+}
+
+function saydone() {
+    document.getElementById("page").style.display = "block";
+
+}
