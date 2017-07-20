2a38cdee44fb7e3d75f978bdd84c05983145b1f2
Julio Lima <julio_flima@hotmail.com>
2017-07-20 02:27:56 -0300

    i changed orientation or touch scroll

--- js/nailsup.js
+++ js/nailsup.js
@@ -36,3 +36,22 @@ setTimeout(function () {
 addEventListener("load", function() {
     window.scrollTo(1, 0);
 }, false);
+
+//other scroll thing
+if( !window.location.hash && window.addEventListener ){
+    window.addEventListener( "load",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+    window.addEventListener( "orientationchange",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+    window.addEventListener( "touchstart",function() {
+         setTimeout(function(){
+             window.scrollTo(0, 0);
+         }, 0);
+     });
+}
