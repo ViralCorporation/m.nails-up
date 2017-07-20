811959123621a011aa3b3cd935824e95115df4bf
melissecabral <melissecabral@gmail.com>
2017-07-20 12:48:44 -0300

    animated footer menu

--- js/nailsup.js
+++ js/nailsup.js
@@ -20,38 +20,70 @@ function saydone() {
     document.getElementById("page").style.display = "flex";
 
 }
-function sendEmail(){
+
+function sendEmail() {
     window.location.href = "mailto:info@nailsup.com.br?subject=Subject&body=message%20goes%20here";
 }
-function sendEmailHelp(){
+
+function sendEmailHelp() {
     window.location.href = "mailto:help@nailsup.com.br?subject=Subject&body=message%20goes%20here";
 }
 
 
 setTimeout(function () {
-  window.scrollTo(0, 1);
+    window.scrollTo(0, 1);
 }, 1000);
 
 
-addEventListener("load", function() {
+addEventListener("load", function () {
     window.scrollTo(1, 0);
 }, false);
 
 //other scroll thing
-if( !window.location.hash && window.addEventListener ){
-    window.addEventListener( "load",function() {
-        setTimeout(function(){
+if (!window.location.hash && window.addEventListener) {
+    window.addEventListener("load", function () {
+        setTimeout(function () {
             window.scrollTo(0, 0);
         }, 0);
     });
-    window.addEventListener( "orientationchange",function() {
-        setTimeout(function(){
+    window.addEventListener("orientationchange", function () {
+        setTimeout(function () {
             window.scrollTo(0, 0);
         }, 0);
     });
-    window.addEventListener( "touchstart",function() {
-         setTimeout(function(){
-             window.scrollTo(0, 0);
-         }, 0);
-     });
+    window.addEventListener("touchstart", function () {
+        setTimeout(function () {
+            window.scrollTo(0, 0);
+        }, 0);
+    });
 }
+$(function () {
+    var animation = 'animated bounceIn';
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    $('a.home').on('click', function () {
+        $('#home-icon').addClass(animation).one(animationEnd, function () {
+            $(this).removeClass(animation);
+        });
+    });
+    $('a.schedulling').on('click', function () {
+        $('#schedulling-icon').addClass(animation).one(animationEnd, function () {
+            $(this).removeClass(animation);
+        });
+    });
+    $('a.services').on('click', function () {
+        $('#services-icon').addClass(animation).one(animationEnd, function () {
+            $(this).removeClass(animation);
+        });
+    });
+    $('a.gift').on('click', function () {
+        $('#gift-icon').addClass(animation).one(animationEnd, function () {
+            $(this).removeClass(animation);
+        });
+    });
+    $('a.help').on('click', function () {
+        $('#help-icon').addClass(animation).one(animationEnd, function () {
+            $(this).removeClass(animation);
+        });
+    });
+});
