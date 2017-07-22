b579f5c3285d0152b1d6a61662035f108e56077f
melissecabral <melissecabral@gmail.com>
2017-07-22 15:42:37 -0300

    removing  ; on end of cards

--- js/nailsup.js
+++ js/nailsup.js
@@ -87,86 +87,6 @@ $(function () {
     });
 });
 
-$(function () {
-    var dialog = document.querySelector('dialog');
-    var title = document.getElementById("title-more");
-    var info = document.getElementById("content-service-info");
-
-    var showDialogButtonBronze = document.querySelector('#show-dialog-bronze');
-    var showDialogButtonBronze1 = document.querySelector('#show-dialog-bronze_1');
-    var showDialogButtonBronze2 = document.querySelector('#show-dialog-bronze_2');
-    var showDialogButtonEyes = document.querySelector('#show-dialog-eyes');
-    var showDialogButtonDep = document.querySelector('#show-dialog-dep');
-    var showDialogButtonMassage = document.querySelector('#show-dialog-massage');
-    var showDialogButtonSprayTan = document.querySelector('show-dialog-sprayTan');
-    var showDialogButtonDetox = document.querySelector('#show-dialog-detox');
-    var showDialogButtonPac = document.querySelector('#show-dialog-pac');
-
-    if (!dialog.showModal) {
-        dialogPolyfill.registerDialog(dialog);
-    }
-    dialog.querySelector('.close').addEventListener('click', function () {
-        dialog.close();
-    });
-
-    showDialogButtonBronze.addEventListener('click', function () {
-
-        title.innerHTML = (document.getElementById("bronze-title").innerHTML);
-        info.innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-    showDialogButtonBronze1.addEventListener('click', function () {
-
-        title.innerHTML = (document.getElementById("bronze-title1").innerHTML);
-        info.innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-
-    showDialogButtonDep.addEventListener('click', function () {
-
-        title.innerHTML = (document.getElementById("dep-title").innerHTML);
-        info.innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-
-    showDialogButtonMassage.addEventListener('click', function () {
-
-        title.innerHTML = (document.getElementById("massage-title").innerHTML);
-        title.innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-    if (!dialog.showModal) {
-        dialogPolyfill.registerDialog(dialog);
-    }
-    dialog.querySelector('.close').addEventListener('click', function () {
-        dialog.close();
-    });
-
-    showDialogButtonEyes.addEventListener('click', function () {
-        document.getElementById("title-more").innerHTML = (document.getElementById("eyes-title").innerHTML);
-        document.getElementById("content-service-info").innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-
-    showDialogButtonDetox.addEventListener('click', function () {
-        document.getElementById("title-more").innerHTML = (document.getElementById("detox-title").value);
-        document.getElementById("content-service-info").innerHTML = (document.getElementById("content-service-info").value);
-        dialog.showModal();
-    });
-    showDialogButtonPac.addEventListener('click', function () {
-
-        document.getElementById("title-more").innerHTML = (document.getElementById("pac-title").innerHTML);
-        document.getElementById("content-service-info").innerHTML = (document.getElementById("content-service-info").innerHTML);
-        dialog.showModal();
-    });
-    showDialogButtonSprayTan.addEventListener('click', function () {
-
-        document.getElementById("title-more").innerHTML = (document.getElementById("spray-tan-title").value);
-        document.getElementById("content-service-info").innerHTML = (document.getElementById("content-service-info").value);
-        dialog.showModal();
-    });
-});
-
 class Service {
     /* Guide Types of Service
         PACK
@@ -200,8 +120,9 @@ list_services[9] = new Service("Massagem", "Maquiagem basica", "50", "MASSAGE");
 list_services[10] = new Service("Suco Detox", "genginbre", "12", "DETOX");
 var started = false;
 
-function createServiceCard(service_name, descrition, price, infoservice) {
-    var service_card = '<div class="demo-card-wide mdl-card mdl-shadow--2dp animated fadeIn"><div class="mdl-card__title" id="spray_tan"><h2 class="mdl-card__title-text">' + service_name + '</h2></div><div class="little_text"> <span class = "mdl-card__supporting-text" >' + descrition + '...</span><span id ="show-dialog-bronze" type = "button" class = "more mdl-button" > More </span> <div class = "mdl-card__actions mdl-card--border" ><a id = "value"> R$ 3~15 </a> <a> | </a> <a id = "time"> 30 min </a> </div> </div> <div class="button mdl-js-button mdl-button--fab mdl-js-ripple-effect "><i class="contact material-icons md-24 secondary-color"> add_circle </i></div><dialog class = "mdl-dialog bronze" ><h4 class = "mdl-dialog__title" > Service < /h4> <div class = "mdl-dialog__content" ><p >More info about the services here </p> </div> <div class = "mdl-dialog__actions" ><button type="button" class ="mdl-button close"> Close </button></div> </dialog> </div>;'
+function createServiceCard(service_name, descrition, price, infoservice, id) {
+
+    var service_card = '<div class="demo-card-wide mdl-card mdl-shadow--2dp animated fadeIn"><div class="mdl-card__title" id="spray_tan"><h2 class="mdl-card__title-text">' + service_name + '</h2></div><div class="little_text"> <span class = "mdl-card__supporting-text" >' + descrition + '...</span><span id ="show-dialog-' +id + '" type = "button" class = "more mdl-button" > More </span> <div class = "mdl-card__actions mdl-card--border" ><a id = "value"> R$ 3~15 </a> <a> | </a> <a id = "time"> 30 min </a> </div> </div> <div class="button mdl-js-button mdl-button--fab mdl-js-ripple-effect "><i class="contact material-icons md-24 secondary-color"> add_circle </i></div><dialog class = "mdl-dialog bronze" ><h4 class = "mdl-dialog__title" > Service < /h4> <div class = "mdl-dialog__content" ><p >More info about the services here </p> </div> <div class = "mdl-dialog__actions" ><button type="button" class ="mdl-button close"> Close </button></div> </dialog> </div>'
     return service_card;
 }
 
@@ -209,7 +130,7 @@ function createAllServiceCardsOnArray() {
     if (started == false) {
         for (i = 0; i < list_services.length; i++) {
             console.log(list_services[i]);
-            var card = createServiceCard(list_services[i].name_service, list_services[i].descrition, list_services[i].price, list_services[i].infoservice);
+            var card = createServiceCard(list_services[i].name_service, list_services[i].descrition, list_services[i].price, list_services[i].infoservice, i);
             if (list_services[i].type_service == "SPRAY-TAN") {
                 $('div.spraytan').append(card);
             } else if (list_services[i].type_service == "EYES") {
@@ -231,6 +152,65 @@ function createAllServiceCardsOnArray() {
         }
     }
 }
+$(function () {
+    var dialog = document.querySelector('dialog');
+    var title = document.getElementById("title-more");
+    var info = document.getElementById("content-service-info");
+
+    var showDialogButton_0 = document.querySelector('#show-dialog-0');
+    var showDialogButton_1 = document.querySelector('#show-dialog-1');
+    var showDialogButton_2 = document.querySelector('#show-dialog-2');
+    var showDialogButton_3 = document.querySelector('#show-dialog-3');
+    var showDialogButton_4 = document.querySelector('#show-dialog-4');
+    var showDialogButton_5 = document.querySelector('#show-dialog-5');
+    var showDialogButton_6 = document.querySelector('#show-dialog-6');
+
+    if (!dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.querySelector('.close').addEventListener('click', function () {
+        dialog.close();
+    });
+
+    showDialogButton_0.addEventListener('click', function () {
+
+        title.innerHTML = (list_services[0].name_service);
+        info.innerHTML = (list_services[0].descrition);
+        dialog.showModal();
+    });
+    showDialogButton_1.addEventListener('click', function () {
+
+        title.innerHTML = (list_services[1].name_service);
+        info.innerHTML = (list_services[1].descrition);
+        dialog.showModal();
+    });
+    showDialogButton_2.addEventListener('click', function () {
+
+        title.innerHTML = (document.getElementById("bronze-title1").innerHTML);
+        info.innerHTML = (document.getElementById("content-service-info").innerHTML);
+        dialog.showModal();
+    });
+
+    showDialogButton_3.addEventListener('click', function () {
+
+        title.innerHTML = (document.getElementById("dep-title").innerHTML);
+        info.innerHTML = (document.getElementById("content-service-info").innerHTML);
+        dialog.showModal();
+    });
+
+    showDialogButton_4.addEventListener('click', function () {
+
+        title.innerHTML = (document.getElementById("massage-title").innerHTML);
+        title.innerHTML = (document.getElementById("content-service-info").innerHTML);
+        dialog.showModal();
+    });
+    if (!dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.querySelector('.close').addEventListener('click', function () {
+        dialog.close();
+    });
+});
 var car_sch = Array(10);
 
 //Hidding and showing the Services Presentation
@@ -245,6 +225,7 @@ function show_s_prst() {
     document.documentElement.style.setProperty(`--state_s_tbar`, 'none');
     document.documentElement.style.setProperty(`--state_s_box`, 'none');
 }
+
 window.onload = initPage;
 
 function initPage() {
