7c97511f7dbd4830e3624946b7a1148aa0ba9924
Julio Lima <julio_flima@hotmail.com>
2017-07-22 18:45:33 -0300

    added julio js and css

--- js/nailsup.js
+++ js/nailsup.js
@@ -329,16 +329,20 @@ $(function () {
 var car_sch = Array(10);
 
 //Hidding and showing the Services Presentation
-function hide_s_prst() {
-    document.documentElement.style.setProperty(`--state_s_prst`, 'none');
-    document.documentElement.style.setProperty(`--state_s_tbar`, 'block');
-    document.documentElement.style.setProperty(`--state_s_box`, '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)');
+function hide_s_prst(){
+	console.log("hide_s_prst");
+	 document.documentElement.style.setProperty(`--state_s_prst`, 'none');
+	 document.documentElement.style.setProperty(`--state_s_tbar`, 'contents');
+	 document.documentElement.style.setProperty(`--state_s_tab`, 'flex');
+	 document.documentElement.style.setProperty(`--state_s_box`, '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)');
 }
 
-function show_s_prst() {
-    document.documentElement.style.setProperty(`--state_s_prst`, 'contents');
-    document.documentElement.style.setProperty(`--state_s_tbar`, 'none');
-    document.documentElement.style.setProperty(`--state_s_box`, 'none');
+function show_s_prst(){
+	console.log("show_s_prst");
+	 document.documentElement.style.setProperty(`--state_s_prst`, 'contents');
+	 document.documentElement.style.setProperty(`--state_s_tbar`, 'flex');
+	 document.documentElement.style.setProperty(`--state_s_tab`, 'none');
+	 document.documentElement.style.setProperty(`--state_s_box`, 'none');
 }
 
 window.onload = initPage;
