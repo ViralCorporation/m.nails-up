a11ce7858b68e98073b8efcc1394979ae07ba073
Melisse Cabral <melissecabral@gmail.com>
2017-09-14 17:00:19 -0300

    adding services

--- js/nailsup.js
+++ js/nailsup.js
@@ -43,17 +43,52 @@ Guide to constructor
     name of service, Descrition of service, price, categorie
 */
 
-list_services[0] = new Service("Bronzeamento Natural", "Bronzeamento natural com uso pasma", 39.00, "15 min", "SPRAY_TAN");
-list_services[1] = new Service("Bronzeamento Natural na Galaxia", "Bronzeamento natural com uso pasma", 39.00, '15min', "SPRAY_TAN");
-list_services[2] = new Service("Bronzeamento Natural na Sol", "Bronzeamento natural com uso pasma", 60.50, "25 min", "SPRAY_TAN");
-list_services[3] = new Service("Manicure", "Pintura de unhas", 50.00, "15min", "NAILS");
-list_services[4] = new Service("Make Up", "Maquiagem basica", 50.00, "30 min", "MAKEUP");
-list_services[5] = new Service("Bronzeamento Natural na Lua", "Bronzeamento natural com uso pasma", 50.00, "20 min", "SPRAY_TAN");
-list_services[6] = new Service("Sombras", "Maquiagem basica", 50.00, "20 min", "EYES");
-list_services[7] = new Service("Depilação Buço", "Maquiagem basica", 50.00, "10 min", "WAX");
-list_services[8] = new Service("Pacote Depilação completa", "Maquiagem basica", 50.00, "2 h", "PACK");
-list_services[9] = new Service("Massagem", "Maquiagem basica", 50.00, "1 h", "MASSAGE");
-list_services[10] = new Service("Suco Detox", "genginbre", 12.00, "5 min", "DETOX");
+list_services[0] = new Service("Colocação de Cílios 2D ", "Volume e delicadeza", 90.00, "45-60 min", "EYES");
+list_services[1] = new Service("Aplicação de Cílios Fio a Fio", "(Glamour e sensual", 115.00, "", "EYES");
+list_services[2] = new Service("Design Sobrancelhas", "Pigmentação com henna", 35.00, "", "EYES");
+list_services[3] = new Service("Design Sobrancelhas", "Pigmentação com tinta", 65.00, "", "EYES");
+
+list_services[5] = new Service("Depilação Buço", "", 15.00, "", "WAX");
+list_services[6] = new Service("Depilação Sobrancelhas", "", 30.00, "", "WAX");
+list_services[7] = new Service("Depilação Axila", "", 20.00, "", "WAX");
+list_services[8] = new Service("Depilação Virilha Contorno", "", 28.00, "", "WAX");
+list_services[9] = new Service("Depilação Contorno", "Primavera / Verão", 40.00, "", "WAX");
+list_services[10] = new Service("Depilação Meia Perna", "", 32.00, "", "WAX");
+list_services[11] = new Service("Depilação Full Brazilian Bikini", "", 45.00, "", "WAX");
+
+
+list_services[4] = new Service("Manicure - Basic", "", 28.00, "", "NAILS");
+list_services[12] = new Service("Pedicure - Basic", "", 35.00, "", "NAILS");
+list_services[13] = new Service("Manicure NAILS-UP DTOX SPA", "Esfoliação com sais, toalha aquecida, banho de parafina e hidratação importada", 45.00, "", "NAILS");
+list_services[14] = new Service("Pedicure NAILS-UP DTOX SPA", "Esfoliação, com banheira aquecida, sais minerais,massagem com pedras vulcânicas, hidratação importada", 65.00, "", "NAILS");
+list_services[15] = new Service("French Style", "", 35.00, "", "NAILS");
+list_services[16] = new Service("Esmaltalção Best of Gel OPI", "", 70.00, "", "NAILS");
+list_services[17] = new Service("Esmaltação Nacional Best of Brasil", "", 20.00, "", "NAILS");
+list_services[18] = new Service("French Style Best of Gel", "", 35.00, "", "NAILS");
+list_services[19] = new Service("Aplicação ou Manutenção Unha Postiça Individual", "", 20.00, "", "NAILS");
+list_services[20] = new Service("Acrílico Gel", "", 170.00, "", "NAILS");
+list_services[21] = new Service("Fibra de Vidro", "", 220.00, "", "NAILS");
+list_services[22] = new Service("Manutenção Completa", "", 75.00, "", "NAILS");
+list_services[23] = new Service("Aplicação Gel OPI", "", 65.00, "", "NAILS");
+list_services[24] = new Service("Esmaltação Infantil com Arte Personalizada e Esmaltes Hipoalergênico", "", 20.00, "", "NAILS");
+list_services[25] = new Service("Esmaltação & Parafina", "", 35.00, "", "NAILS");
+list_services[26] = new Service("Manicure & Parafina", "", 35.00, "", "NAILS");
+list_services[27] = new Service("Remoção Porcelana", "", 60.00, "", "NAILS");
+list_services[28] = new Service("SPA NAILS-UP Manicure & Stone / Parafina & Massagem", "10 min", 55.00, "", "NAILS");
+list_services[29] = new Service("SPA NAILS-UP Pedicure & Stone / Parafina & Massagem", "10 min", 65.00, "", "NAILS");
+list_services[30] = new Service("Manicure & Pedicure", "", 55.00, "", "NAILS");
+list_services[31] = new Service("Monthly Package", "", 55.00, "", "NAILS");
+
+list_services[32] = new Service("Vitamina DAY & DTOX SPA Massagem", "", 0.00, "", "DETOX");
+list_services[21] = new Service("Vitamina DAY & DTOX SPA Manicure & Pedicure", "", 0.00, "", "DETOX");
+list_services[22] = new Service("Vitamina DAY & DTOX SPA Bronze & Hidratação", "", 00, "", "DETOX");
+list_services[23] = new Service("Vitamina DAY & DTOX SPA Depilação & Hidratação", "", 00, "", "DETOX");
+list_services[24] = new Service("Nutrição Análises Day  & Semanal ", "", 20.00, "", "DETOX");
+list_services[25] = new Service("Yoga Day & Vitamina e Análise de Nutrição ", "", 00, "", "DETOX");
+list_services[26] = new Service("Focus Vitamina Day-  DTOX & DAY SPA", "", 00, "", "DETOX");
+
+list_services[27] = new Service("Bronzeamento Natural Solar 90 mins  seguido de esfoliação profunda ", "", 65.00, "", "SPRAY_TAN");
+
 
 
 var started = false;
@@ -264,7 +299,7 @@ function sendEmail() {
 // CREATE SERVICE CARD TO BE LISTED ON CATEGORIES
 function createServiceCard(service, id) {
 
-    var service_card = '<div class="demo-card-wide mdl-card mdl-shadow--2dp animated fadeIn" style="user-select: none"><div class="mdl-card__title" id="spray_tan"><h2 class="mdl-card__title-text">' + service.name_service + '</h2></div><div class="little_text"> <span class = "mdl-card__supporting-text" >' + service.descrition + '...</span><span id ="show-dialog-' + service.hash + '" type = "button" class = "more mdl-button dialog_button" > More </span> <div class = "mdl-card__actions mdl-card--border" ><a id = "value"> R$ ' + service.price + ' </a> <a> | </a> <a id = "time"> ' + service.duration + ' </a> </div> </div> <div class="button mdl-js-button mdl-button--fab mdl-js-ripple-effect add_button" id="add_service_' + service.hash + '"><i class="contact material-icons md-18 add-remove-color"> add </i></div><dialog class = "mdl-dialog service-box-' + service.hash + '" ><h4 class = "mdl-dialog_title service-title"' + service.name_service + '"</h4> <div class = "mdl-dialogcontent" ><p class="service-description">' + service.descrition + '</p></div><div class="mdl-dialog_actions" ><button type="button" class ="mdl-button close"> Close </button></div> </dialog></div>'
+    var service_card = '<div class="demo-card-wide mdl-card mdl-shadow--2dp animated fadeIn" style="user-select: none"><div class="mdl-card__title" id="spray_tan"><h2 class="mdl-card__title-text">' + service.name_service + '</h2></div><div class="little_text"> <span class = "mdl-card__supporting-text" >' + service.descrition + '.</span><div class = "mdl-card__actions mdl-card--border" ><a id = "value"> R$ ' + service.price + ' </a> <a> | </a> <a id = "time"> ' + service.duration + ' </a> </div> </div> <div class="button mdl-js-button mdl-button--fab mdl-js-ripple-effect add_button" id="add_service_' + service.hash + '"><i class="contact material-icons md-18 add-remove-color"> add </i></div><dialog class = "mdl-dialog service-box-' + service.hash + '" ><h4 class = "mdl-dialog_title service-title"' + service.name_service + '"</h4> <div class = "mdl-dialogcontent" ><p class="service-description">' + service.descrition + '</p></div><div class="mdl-dialog_actions" ><button type="button" class ="mdl-button close"> Close </button></div> </dialog></div>'
     return service_card;
 }
 
