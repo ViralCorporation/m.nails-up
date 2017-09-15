f6b5971e180915cc8783b74fd8a59903c85dbb59
Melisse Cabral <melissecabral@gmail.com>
2017-09-14 21:05:03 -0300

    email

--- js/nailsup.js
+++ js/nailsup.js
@@ -291,8 +291,8 @@ function gerateSubjectEmailSdl() {
 
 function sendEmail() {
     if (current_cart.length >= 0) {
-        Email.send('naislupdetoxbar@gmail.com', 'melissecabral@gmail.com', gerateSubjectEmailSdl(), gerateBodyEmailSdl(), 'smtp.gmail.com',
-            'naislupdetoxbar@gmail.com', "Projectnumber1");
+        Email.send('nailsupdetoxbar@gmail.com', 'melissecabral@gmail.com', gerateSubjectEmailSdl(), gerateBodyEmailSdl(), 'smtp.gmail.com',
+            {token: "89d92453-935a-4144-aaff-78c59f77f9d6"});
         removingAllFromCart();
         console.log("email sended")
     }
